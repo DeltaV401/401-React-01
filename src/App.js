ff2ddbcbec9e76562c580828e66521482f3ad723
Jon Struve <ocdammo3@gmail.com>
2019-10-21 11:49:00 -0500

    Added base for react

--- src/App.js
+++ src/App.js
@@ -2,24 +2,75 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <h1>React Word Flipper!</h1>
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+  <footer>&copy;DeltaV 2019</footer>
+  )
+}
+
+function Logo() {
+  return (
+    <img src={logo} className="App-logo" alt="logo" />
+  )
+}
+
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      words: 'DeltaV does React!',
+    };
+  }
+
+  handleWords = e => {
+    let words = e.target.value;
+    this.setState({
+      words: words,
+    });
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+    
+    this.setState(state => { 
+      let words = this.state.words.split('').reverse().join('');
+      
+      return {
+        words,
+        count: state.count + 1,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <main>
+        <h3>{this.state.words}</h3>
+        <h4>Count: {this.state.count}</h4>
+        <input onChange={this.handleWords} placeholder="Something to flip"/>
+        <button onClick={this.handleClick}id="submit">Flip it!</button>
+      </main>
+    )
+  }
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <Main />
+      <Logo />
+      <Footer />
+    </React.Fragment>
   );
 }
 
